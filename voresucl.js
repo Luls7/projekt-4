e2b644b1c8c3abcdbd80752a95ebd75f43ae320b
mathildenicoline <mathilde.nicoline@gmail.com>
2023-04-17 10:57:15 +0200

    mathilde countdown

--- voresucl.js
+++ voresucl.js
@@ -28,6 +28,38 @@ function kørSlide() {
 }
 
 
+
+//Mathilde
+var countDownDate = new Date("Jun 1, 2023 00:00:00").getTime();
+
+// Nedtællingen bliver opdateret hvert sekund
+var x = setInterval(function() {
+
+  // Dato og tid for i dag
+  var now = new Date().getTime();
+
+  // Afstanden mellem nu og nedtællingsdatoen
+  var distance = countDownDate - now;
+
+  // Tidsberegninger for dage, timer, minutter og sekunder
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Udskriv resultatet i et element med id="demo"
+  document.getElementById("ur").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // Når countdown er ovre, vil der står dette...
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("ur").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
 function changeColor(element, color) {
   element.style.backgroundColor = color; 
 }
