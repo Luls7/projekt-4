eb661c3ce48f92ec68deab499cf061ffcea0735d
mathildenicoline <mathilde.nicoline@gmail.com>
2023-04-19 22:40:39 +0200

    fuckt

--- voresucl.js
+++ voresucl.js
@@ -29,6 +29,36 @@ let x = setInterval(function() {
 }, 1000);
 
 
+let countDownDate = new Date("Jun 1, 2023 00:00:00").getTime();
+
+// Nedtællingen bliver opdateret hvert sekund
+let x = setInterval(function() {
+
+  // Dato og tid for i dag
+  let now = new Date().getTime();
+
+  // Afstanden mellem nu og nedtællingsdatoen
+  let distance = countDownDate - now;
+
+  // Tidsberegninger for dage, timer, minutter og sekunder
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Udskriv resultatet i et element med id="demo"
+  document.getElementById("ur").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+
+  // Når countdown er ovre, vil der står dette...
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("ur2").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
 
 function myFunction() {
     var x = document.getElementById("myLinks");
