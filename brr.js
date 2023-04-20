6730055e31140362daec80a4966e5aa0d2ac258e
annafoverskov <119939347+annafoverskov@users.noreply.github.com>
2023-04-20 09:43:05 +0200

    .

--- brr.js
+++ brr.js
@@ -1,5 +1,34 @@
 //Mathilde
-let countDownDate = new Date("Jun 1, 2023 00:00:00").getTime();
+let countDownDate1 = new Date("Jun 1, 2023 00:00:00").getTime();
+
+// Nedtællingen bliver opdateret hvert sekund
+let y = setInterval(function() {
+
+  // Dato og tid for i dag
+  let now = new Date().getTime();
+
+  // Afstanden mellem nu og nedtællingsdatoen
+  let distance = countDownDate1 - now;
+
+  // Tidsberegninger for dage, timer, minutter og sekunder
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Udskriv resultatet i et element med id="demo"
+  document.getElementById("ur1").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // Når countdown er ovre, vil der står dette...
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("ur1").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+//Mathilde
+let countDownDate2 = new Date("Jun 1, 2023 00:00:00").getTime();
 
 // Nedtællingen bliver opdateret hvert sekund
 let x = setInterval(function() {
@@ -8,7 +37,7 @@ let x = setInterval(function() {
   let now = new Date().getTime();
 
   // Afstanden mellem nu og nedtællingsdatoen
-  let distance = countDownDate - now;
+  let distance = countDownDate2 - now;
 
   // Tidsberegninger for dage, timer, minutter og sekunder
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
