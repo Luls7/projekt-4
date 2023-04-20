5bd376d6cb0bbe8705c684aed645138ff47440d5
annafoverskov <119939347+annafoverskov@users.noreply.github.com>
2023-04-20 12:27:16 +0200

    -

--- logind.js
+++ logind.js
@@ -1,29 +1,29 @@
 window.addEventListener("DOMContentLoaded",load);
 
 //Array 
-const loginData = [
+const logindNavne = [
     {"brugernavn":"Studerende1","kodeord":"Studerende1"},
     {"brugernavn":"Studerende2","kodeord":"Studerende2"}
 ];
 
 function load(){
-    console.log("JS VIRKER!");
+    console.log("det virker");
 
     // Eventlistener som reagerer på at man klikker på "login" knappen. (e) for det her event.
     // => arrowfunction (kan få lov at lave en function uden et navn, men den kan ikke kaldes senere, den virker kun lige her)
     // PreventDefault() fordi der er en default på typen submit som vi gerne vil override.
-    document.querySelector("#login-button").addEventListener("click", (e) => {
+    document.querySelector("#logind-knap").addEventListener("click", (e) => {
     e.preventDefault();
 
     // Variabler for at ungå at skrive det i funktionen.
     // queryselector er standarden at bruge, den kan bruges både til at target elementer ved brug af tags, id´er og class
-    const usernameInput = document.querySelector("#username-input").value;
+    const usernameInput = document.querySelector("#brugernavn").value;
     const passwordInput = document.querySelector("#password-input").value;
 
     // vi bruger const fordi at det ikke er noget der skal ændre sig, vi bruger let når vi har en værdig vi ved der skal ændre sig
 
-    for(let i = 0; i < loginData.length; i++){        
-        if (loginData[i].brugernavn == usernameInput && loginData[i].kodeord == passwordInput){
+    for(let i = 0; i < logindNavne.length; i++){        
+        if (logindNavne[i].brugernavn == usernameInput && logindNavne[i].kodeord == passwordInput){
             window.location = "index.html";
             break; // Stopper loopet hvis if-statement er true og den når til break (kan også bruges til andre ting)
         } else {
